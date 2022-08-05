ffb2ed31b8347d652a08acb456151dc8aa4925d8
ekriu <dev@datafiche.com>
2022-08-05 14:39:11 +0300

    db init

---
+++ prisma/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: `eugene.kr@gmail.com`,
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
