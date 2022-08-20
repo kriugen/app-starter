63f764ea2939538b2c9026ae1dba7c44f8a8f403
ekriu <dev@datafiche.com>
2022-08-20 17:12:38 +0300

    profile ssp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { UserProvider } from "@auth0/nextjs-auth0";
 
-import AppBarContainer from '../src/components/AppBarContainer'
+import AppBar from '../src/components/AppBar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <UserProvider>
-    <AppBarContainer />
+    <AppBar />
     <Component {...pageProps} />
   </UserProvider>
 }
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next'
-import { useUser } from '@auth0/nextjs-auth0';
+import { prisma } from '../lib/prisma';
+import { useProfile } from '../src/components/Profile/hooks';
+import { getSession } from '@auth0/nextjs-auth0';
 
-const Profile: NextPage = () => {
-  const { user } = useUser();
-  if (!user) {
-    return <div>Loading</div>;
-  }
+const Profile: NextPage = ({ user }) => {
+  console.log('---USER', user);
   return (
     <>
       <div>
@@ -14,8 +13,38 @@ const Profile: NextPage = () => {
       <div>
         { user?.email }
       </div>
+      {/* <div>
+        Created At
+        { profile?.createdAt }
+      </div>
+      <div>
+        Updated At
+        { profile?.updatedAt }
+      </div> */}
     </>
   )
 }
+
+export const getServerSideProps = async ({ req, res, params }) => {
+  const { user } = getSession(req, res);
+
+  // const id = params.id;
+  // const link = await prisma.link.findUnique({
+  //   where: { id },
+  //   select: {
+  //     id: true,
+  //     title: true,
+  //     category: true,
+  //     url: true,
+  //     imageUrl: true,
+  //     description: true,
+  //   },
+  // });
+  return {
+    props: {
+      user,
+    },
+  };
+};
 
 export default Profile
