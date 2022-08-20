962b148e7b5d1c91c5e3dce2b0dbba8891c9a3bc
ekriu <dev@datafiche.com>
2022-08-20 13:59:45 +0300

    profile email

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,10 +1,20 @@
 import type { NextPage } from 'next'
+import { useUser } from '@auth0/nextjs-auth0';
 
 const Profile: NextPage = () => {
+  const { user } = useUser();
+  if (!user) {
+    return <div>Loading</div>;
+  }
   return (
-    <div>
-      User Profile
-    </div>
+    <>
+      <div>
+        User Profile
+      </div>
+      <div>
+        { user?.email }
+      </div>
+    </>
   )
 }
 
