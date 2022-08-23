d2dfef4ab24392e20a310b643e6e0543e68d4572
ekriu <dev@datafiche.com>
2022-08-23 15:04:08 +0300

    fab

--- src/components/Note/NoteList.tsx
+++ src/components/Note/NoteList.tsx
@@ -3,30 +3,35 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import CommentIcon from '@mui/icons-material/Comment';
+import AddIcon from '@mui/icons-material/Add';
 import IconButton from '@mui/material/IconButton';
-import { ListItemButton } from '@mui/material';
+import { Fab, ListItemButton } from '@mui/material';
 import Link from 'next/link';
 
 export default function NoteList({ notes }) {
   return (
-    <List>
-      {notes.map((note) => (
-        <ListItem
-          key={note.id}
-          secondaryAction={
-            <IconButton aria-label="comment">
-              <CommentIcon />
-            </IconButton>
-          }
-        >
-        
+    <div>
+      <List>
+        {notes.map((note) => (
+          <ListItem
+            key={note.id}
+            secondaryAction={
+              <IconButton aria-label="comment">
+                <CommentIcon />
+              </IconButton>
+            }
+          >
             <Link href={`/note/${note.id}`}>
             <ListItemButton component="a">
                 <ListItemText primary={ note.title } />
             </ListItemButton>
             </Link>
         </ListItem>
-      ))}
-    </List>
+        ))}
+      </List>
+      <Fab sx={{ float: 'right' }} color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+    </div>
   );
 }
