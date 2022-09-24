1ded10a31a036466ed04f14a352872da20dc310c
ekriu <dev@datafiche.com>
2022-09-24 12:31:09 +0300

    test clear endpoint

---
+++ pages/api/test/clear.ts
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { prisma } from '../../../lib/prisma';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+    prisma.note.deleteMany({}).then(r => {
+      res.status(200).json(r);
+    }).catch ((e) => {
+    console.error('+++TEST DELETE ALL ERROR', e);
+    res.status(500).send('Internal Server Error Deleting All Notes');
+  });
+}
