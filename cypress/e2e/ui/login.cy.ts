57697ab1e5933b4286f363546cf76a1d38c9b9a4
ekriu <dev@datafiche.com>
2022-09-25 12:58:27 +0300

    edit note, optimization

--- cypress/e2e/ui/login.cy.ts
+++ cypress/e2e/ui/login.cy.ts
@@ -9,6 +9,13 @@ describe('login', () => {
     cy.reload();
     cy.getBySel('appbar-open-settings').should('contain', Cypress.env('auth0Username'));
   })
+
+  it('displays login error if not logged in visiting protected area', () => {
+    cy.logout();
+    cy.visit('/notes');
+    cy.location("pathname").should("eq", "/notes");
+    cy.getBySel('login-error').should('exist');
+  })
 })
 
 export {}
--- cypress/e2e/ui/notes.cy.ts
+++ cypress/e2e/ui/notes.cy.ts
@@ -1,32 +1,35 @@
 describe('notes', () => {
-  it('displays login error if not logged in', () => {
-      cy.logout();
-      cy.visit('/notes');
-      cy.location("pathname").should("eq", "/notes");
-      cy.getBySel('login-error').should('exist');
-  })
-
-  it('displays notes list if logged in', () => {
-    cy.login();
-    cy.visit('/notes');
-    cy.getBySel('new-note-button').should('exist');
-  })
-
-  it.only('adds note by changing title', () => {
+  before(() => {
     cy.task("db:seed");
     cy.login();
     cy.visit('/notes');
-    cy.getBySel('new-note-button').should('exist');
+  })
 
-    cy.getBySel('note-list-item').then((e) => {
-      const itemCount = e.length;
+  it('adds note by changing title', () => {
+    cy.getBySel('new-note-button').click();
+    cy.getBySel('note-list-item').then((items) => {
+      const itemCount = items.length;
       cy.getBySel('title').type('test note');
       cy.wait(1000);
 
       cy.getBySel('note-list-item')
         .should('have.length', itemCount + 1)
         .last().should('contain', 'test note');
-    });
+      });
+  })
+
+  it('edits note by changing title', () => {
+    cy.getBySel('note-list-item')
+      .first()
+      .click()
+      .then((item) => {
+        const title = item.prop('innerText');
+        cy.getBySel('title').type('23');
+        cy.wait(1000);
+
+        cy.getBySel('note-list-item')
+          .first().should('contain', title + '23');
+        });
   })
 })
 
